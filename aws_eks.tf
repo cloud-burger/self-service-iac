1a863a5977eb70f776f55dc6004792cd1a25fa62
Gabriel Madrid <50714922+Gabrielmadrid73@users.noreply.github.com>
2025-02-15 20:35:29 -0300

    feature/iac - Added kubectl provider and configured it (#3)

--- aws_eks.tf
+++ aws_eks.tf
@@ -24,6 +24,18 @@ provider "kubernetes" {
   }
 }
 
+provider "kubectl" {
+  apply_retry_count      = 15
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
+  }
+}
 
 module "eks" {
   source                                   = "terraform-aws-modules/eks/aws"
